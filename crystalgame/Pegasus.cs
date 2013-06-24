db5931a862528e3566ccb77e1335fb98d59461f5
Ansis Malins <ansis.malins@gmail.com>
2013-06-24 19:31:50 +0300

    Moar physics tweaks.

--- crystalgame/Pegasus.cs
+++ crystalgame/Pegasus.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -16,7 +15,6 @@ namespace crystalgame
             Agility = GetAgility(view);
             Drag = GetDrag(view);
             Lift = GetLift(view);
-            MaxSpeed = GetMaxSpeed(view);
 
             Matrix m = Matrix.Identity;
             m.Rotate(-Angle * 180 / Math.PI);
@@ -35,8 +33,6 @@ namespace crystalgame
         }
 
         public double Lift { get; set; }
-
-        public double MaxSpeed { get; set; }
 
         public bool WingsSpread { get; set; }
 
@@ -58,12 +54,6 @@ namespace crystalgame
             return (double)view.GetValue(LiftProperty);
         }
 
-        public static double GetMaxSpeed(FrameworkElement view)
-        {
-            Guard.ArgumentNotNull(view, "view");
-            return (double)view.GetValue(MaxSpeedProperty);
-        }
-
         public static void SetAgility(FrameworkElement view, double value)
         {
             Guard.ArgumentNotNull(view, "view");
@@ -82,12 +72,6 @@ namespace crystalgame
             view.SetValue(LiftProperty, value);
         }
 
-        public static void SetMaxSpeed(FrameworkElement view, double value)
-        {
-            Guard.ArgumentNotNull(view, "view");
-            view.SetValue(MaxSpeedProperty, value);
-        }
-
         public override void Simulate(World world)
         {
             Vector uFw = new Vector(Math.Cos(Angle), -Math.Sin(Angle));
@@ -100,24 +84,12 @@ namespace crystalgame
             Vector fDragX = -uFw * vFw * Math.Abs(vFw) * Drag.X;
             Vector fDragY = uUp * vUp * Math.Abs(vUp) * Drag.Y;
             Vector fDrag = fDragX + fDragY;
-            // Sanity clamping.
-            if (fDrag.LengthSquared > Velocity.LengthSquared)
-                fDrag = Normal(fDrag) * Velocity.Length;
 
             Vector fGravity = new Vector(0, world.Gravity);
 
             Vector fLift = uUp * vFw * vFw * Lift;
 
             Velocity += (fDrag + fGravity + fLift) * world.Speed;
-
-            // Sanity clamping.
-            /*Velocity = new Vector(
-                double.IsNaN(Velocity.X) ? 0 : Velocity.X,
-                double.IsNaN(Velocity.Y) ? 0 : Velocity.Y);
-            Velocity = new Vector(
-                Math.Min(Math.Max(Velocity.X, -MaxSpeed), MaxSpeed),
-                Math.Min(Math.Max(Velocity.Y, -MaxSpeed), MaxSpeed));*/
-
             Position += Velocity * world.Speed;
 
             if (Angle < -Math.PI) Angle += Math.PI * 2;
@@ -130,15 +102,11 @@ namespace crystalgame
 
         public static readonly DependencyProperty DragProperty
             = DependencyProperty.RegisterAttached(
-            "Drag", typeof(Vector), typeof(Pegasus), new PropertyMetadata(new Vector(0.00001, 0.4)));
+            "Drag", typeof(Vector), typeof(Pegasus), new PropertyMetadata(new Vector(0.00001, 0.2)));
 
         public static readonly DependencyProperty LiftProperty
             = DependencyProperty.RegisterAttached(
             "Lift", typeof(double), typeof(Pegasus), new PropertyMetadata(0.001));
-
-        public static readonly DependencyProperty MaxSpeedProperty
-            = DependencyProperty.RegisterAttached(
-            "MaxSpeed", typeof(double), typeof(Pegasus), new PropertyMetadata(1500.0));
 
         protected override void Render(FrameworkElement view)
         {
